e08cfd04acd6367f16eea0b4a02f373f7137f33d
jessun <jessun.zhang@outlook.com>
2023-02-09 17:00:04 +0800

    update

--- docker_files/devtools/bootstrap.sh
+++ docker_files/devtools/bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-yum install git which  -y
+yum install git which gcc make bison  -y
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-
+source /root/.gvm/scripts/gvm
+gvm listall
+gvm install go1.17.13 -B
+gvm use go1.17.13 --default
 
